ef8776a131c768170cc6faac39eae221b09ca86f
Artieeee <tuomas.palenius@gmail.com>
2014-02-11 13:54:55 +0200

    Update json2csvTest.js
    update to work with refactored version

--- test/json2csvTest.js
+++ test/json2csvTest.js
@@ -3,132 +3,89 @@
 //   Other reporters e.g. json, nyan, tap, list
 // For documentation visit: http://visionmedia.github.io/mocha/
 
-const assert = require("assert")
-  , j2csv = require("../json2csv")
+var assert = require("assert")
+  , j2csv = require("../lib/jsonToCSV");
 
-describe('j2csv', function() {
+describe ('j2csv', function() {
 
-  describe('convert', function() {
+  describe ('convert', function() {
 
-    it('should not crash when jsonObj is empty', function() {
-      const j = new j2csv()
-          , test = {name: 'foo', age: 10}
-          , result = j.convert()
-      assert.equal(result, undefined)
+    it ('should not crash when jsonObj is empty', function(){
+      var j = new j2csv.jsonToCSV()
+	    , test = {name: 'foo', age: 10}
+        , result = j.convert();
+      assert.equal(result, undefined);
     })
 
-    it('should convert test correctly', function() {
-      const j = new j2csv()
+    it ('should convert test correctly', function(){
+      var j = new j2csv.jsonToCSV()
           , test = {name: 'foo', age: 10}
-          , result = j.convert([test])
-      assert.equal(result, "\"name\",\"age\"\n\"foo\",10\n")
+          , result = j.convert([test]);
+      assert.equal(result, "\"name\",\"age\"\n\"foo\",10\n");
     })
 
-    it('should convert test correctly when separator is ;', function() {
-      const j = new j2csv(';')
+    it ('should convert test correctly when separator is ;', function(){
+      var j = new j2csv.jsonToCSV(';')
           , test = {name: 'foo', age: 10}
-          , result = j.convert([test])
-      assert.equal(result, "\"name\";\"age\"\n\"foo\";10\n")
+          , result = j.convert([test]);
+      assert.equal(result, "\"name\";\"age\"\n\"foo\";10\n");
     })
 
-    it('should convert test correctly when line-end is ;', function() {
-      const j = new j2csv(',', ';')
+    it ('should convert test correctly when line-end is ;', function(){
+      var j = new j2csv.jsonToCSV(',',';')
           , test = {name: 'foo', age: 10}
-          , result = j.convert([test])
-      assert.equal(result, "\"name\",\"age\";\n\"foo\",10;\n")
+          , result = j.convert([test]);
+      assert.equal(result, "\"name\",\"age\";\n\"foo\",10;\n");
     })
-
   })
 
-  describe('buildFieldNames', function() {
-
-    it('use key-names when empty fieldnames (undefined) are given', function() {
-      const j = new j2csv(',', ';')
+  describe ('buildFieldNames', function() {
+    it ('use key-names when empty fieldnames (undefined) are given', function(){
+      var j = new j2csv.jsonToCSV(',',';')
           , jsonObjArray = [{name: 'foo', age: 10}]
           , fieldNames = undefined
-          , result = j.buildFieldNames(jsonObjArray, fieldNames)
-      assert.equal(result, "\"name\",\"age\"")
+          , result = j.buildFieldNames(jsonObjArray, fieldNames);
+      assert.equal(result, "\"name\",\"age\"");
     })
 
-    it('use key-names when empty fieldnames (null) are given', function() {
-      const j = new j2csv(',', ';')
+    it ('use key-names when empty fieldnames (null) are given', function(){
+      var j = new j2csv.jsonToCSV(',',';')
           , jsonObjArray = [{name: 'foo', age: 10}]
           , fieldNames = null
-          , result = j.buildFieldNames(jsonObjArray, fieldNames)
-      assert.equal(result, "\"name\",\"age\"")
+          , result = j.buildFieldNames(jsonObjArray, fieldNames);
+      assert.equal(result, "\"name\",\"age\"");
     })
 
-    it('use key-names when empty fieldnames (empty array) are given', function() {
-      const j = new j2csv(',', ';')
+    it ('use key-names when empty fieldnames (empty array) are given', function(){
+      var j = new j2csv.jsonToCSV(',',';')
           , jsonObjArray = [{name: 'foo', age: 10}]
           , fieldNames = []
-          , result = j.buildFieldNames(jsonObjArray, fieldNames)
-      assert.equal(result, "\"name\",\"age\"")
+          , result = j.buildFieldNames(jsonObjArray, fieldNames);
+      assert.equal(result, "\"name\",\"age\"");
     })
 
-    it('use key-names when fieldnames are given given as string (should be an array)', function() {
-      const j = new j2csv(',', ';')
+    it ('use key-names when fieldnames are given given as string (should be an array)', function(){
+      var j = new j2csv.jsonToCSV(',',';')
           , jsonObjArray = [{name: 'foo', age: 10}]
           , fieldNames = "name, age"
-          , result = j.buildFieldNames(jsonObjArray, fieldNames)
-      assert.equal(result, "\"name\",\"age\"")
+          , result = j.buildFieldNames(jsonObjArray, fieldNames);
+      assert.equal(result, "\"name\",\"age\"");
     })
 
-    it('build correct fieldnames', function() {
-      const j = new j2csv(',', ';')
+    it ('build correct fieldnames', function(){
+      var j = new j2csv.jsonToCSV(',',';')
           , jsonObjArray = [{name: 'foo', age: 10}]
           , fieldNames = ["name"]
-          , result = j.buildFieldNames(jsonObjArray, fieldNames)
-      assert.equal(result, "\"name\"")
+          , result = j.buildFieldNames(jsonObjArray, fieldNames);
+      assert.equal(result, "\"name\"");
     })
 
-    it('build correct fieldnames', function() {
-      const j = new j2csv(',', ';')
+    it ('build correct fieldnames', function(){
+      var j = new j2csv.jsonToCSV(',',';')
           , jsonObjArray = [{name: 'foo', age: 10}]
           , fieldNames = ["name", "age"]
-          , result = j.buildFieldNames(jsonObjArray, fieldNames)
-      assert.equal(result, "\"name\",\"age\"")
+          , result = j.buildFieldNames(jsonObjArray, fieldNames);
+      assert.equal(result, "\"name\",\"age\"");
     })
-
   })
-  
-  describe('objToCSVString', function() {
-
-    it('convert object with string and number correctly to csv string when no separator and line-end are given', function() {
-      const j = new j2csv()
-          , jsonObj = {name: 'foo', age: 10}
-          , result = j.objToCSVString(jsonObj)
-      assert.equal(result, "\"foo\",10")
-    })
-
-    it('convert object with null', function() {
-      const j = new j2csv()
-          , jsonObj = {name: null, age: 10}
-          , result = j.objToCSVString(jsonObj)
-      assert.equal(result, ",10")
-    })
-
-    it('convert object with undefined', function() {
-      const j = new j2csv()
-          , jsonObj = {name: undefined, age: 10}
-          , result = j.objToCSVString(jsonObj)
-      assert.equal(result, ",10")
-    })
-
-    it('convert object with empty string', function() {
-      const j = new j2csv()
-          , jsonObj = {name: '', age: 10}
-          , result = j.objToCSVString(jsonObj)
-      assert.equal(result, "\"\",10")
-    })
-
-    it('convert an array', function() {
-      const j = new j2csv()
-          , jsonObj = {keywords: ['foo', 'bar']}
-          , result = j.objToCSVString(jsonObj)
-      assert.equal(result, "\"foo,bar\"")
-    })
-
-  })
-
 })
